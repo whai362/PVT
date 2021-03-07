5def115d96a82122634a820974aae96472ac1571
whai362 <wangwenhai362@163.com>
2021-03-07 20:41:25 +0800

    detection

---
+++ detection/configs/retinanet_pvt_s_fpn_1x_coco.py
@@ -0,0 +1,28 @@
+_base_ = [
+    '_base_/models/retinanet_r50_fpn.py',
+    '_base_/datasets/coco_detection.py',
+    '_base_/default_runtime.py'
+]
+model = dict(
+    pretrained='pretrained/pvt_small.pth',
+    backbone=dict(
+        type='pvt_small',
+        style='pytorch'),
+    neck=dict(
+        type='FPN',
+        in_channels=[64, 128, 320, 512],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_input',
+        num_outs=5))
+# optimizer
+optimizer = dict(type='AdamW', lr=0.0001, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11])
+total_epochs = 12
