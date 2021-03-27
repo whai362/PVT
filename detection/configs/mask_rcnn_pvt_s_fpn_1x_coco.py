d138c08c2c3e0e4e34983d62254fd293efab4050
whai362 <wangwenhai@163.com>
2021-03-27 10:56:36 +0800

    update

---
+++ detection/configs/mask_rcnn_pvt_s_fpn_1x_coco.py
@@ -0,0 +1,27 @@
+_base_ = [
+    '../configs/_base_/models/mask_rcnn_r50_fpn.py',
+    '../configs/_base_/datasets/coco_instance.py',
+    # '../configs/_base_/schedules/schedule_1x.py',
+    '../configs/_base_/default_runtime.py'
+]
+model = dict(
+    pretrained='pretrained/pvt_small.pth',
+    backbone=dict(
+        type='pvt_small',
+        style='pytorch'),
+    neck=dict(
+        type='FPN',
+        in_channels=[64, 128, 320, 512],
+        out_channels=256,
+        num_outs=5))
+# optimizer
+optimizer = dict(type='AdamW', lr=0.0002, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11])
+total_epochs = 12
