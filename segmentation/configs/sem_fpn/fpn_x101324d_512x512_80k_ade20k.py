41a2f01e6e8f8293e1ba405a6d4a759452756f5c
xieenze <Johnny_ez@163.com>
2021-05-31 18:55:55 +0800

    update

--- segmentation/configs/sem_fpn/fpn_x101324d_512x512_80k_ade20k.py
+++
@@ -1,7 +0,0 @@
-_base_ = './fpn_r50_512x512_80k_ade20k.py'
-model = dict(pretrained='open-mmlab://resnext101_32x4d',
-             backbone=dict(
-                 type='ResNeXt',
-                 depth=101,
-                 groups=32,
-                 base_width=4))
