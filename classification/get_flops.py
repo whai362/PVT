075311981cebde0bf52121b1ba40feda71fbd649
whai362 <wangwenhai@163.com>
2021-06-30 14:57:36 +0800

    update get flops

--- classification/get_flops.py
+++ classification/get_flops.py
@@ -1,104 +1,96 @@
-import torchvision.models as models
+import argparse
+import torch
+from timm.models import create_model
 import pvt
 import pvt_v2
-import torch
-from mmcv.cnn.utils.flops_counter import get_model_complexity_info, flops_to_string, params_to_string
+
+try:
+    from mmcv.cnn import get_model_complexity_info
+    from mmcv.cnn.utils.flops_counter import get_model_complexity_info, flops_to_string, params_to_string
+except ImportError:
+    raise ImportError('Please upgrade mmcv to >0.6.2')
 
 
-def mha_flops(h, w, dim, num_heads):
-    dim_h = dim / num_heads
-    n = h * w
-    f1 = n * dim_h * n * num_heads
-    f2 = n * n * dim_h * num_heads
-    return f1 + f2
+def parse_args():
+    parser = argparse.ArgumentParser(description='Get FLOPS of a classification model')
+    parser.add_argument('model', help='train config file path')
+    parser.add_argument(
+        '--shape',
+        type=int,
+        nargs='+',
+        default=[224, 224],
+        help='input image size')
+    args = parser.parse_args()
+    return args
 
 
-def sra_flops(h, w, r, dim, num_heads):
-    dim_h = dim / num_heads
-    n1 = h * w
-    n2 = h / r * w / r
-
-    f1 = n1 * dim_h * n2 * num_heads
-    f2 = n1 * n2 * dim_h * num_heads
-
-    return f1 + f2
+def sra_flops(h, w, r, dim):
+    return 2 * h * w * (h // r) * (w // r) * dim
 
 
-def lisra_flops(h, w, dim):
+def li_sra_flops(h, w, dim):
     return 2 * h * w * 7 * 7 * dim
 
 
-def get_vit_flops(net, input_shape, patch_size):
-    flops, params = get_model_complexity_info(net, input_shape, as_strings=False)
-    _, H, W = input_shape
-    stage = mha_flops(H // patch_size, W // patch_size,
-                      net.blocks[0].attn.dim,
-                      net.blocks[0].attn.num_heads) * len(net.blocks)
-    flops += stage
+def get_flops(model, input_shape):
+    flops, params = get_model_complexity_info(model, input_shape, as_strings=False)
+    if 'pvt' in model.name:
+        _, H, W = input_shape
+        if 'li' in model.name:  # calculate flops of PVTv2_li
+            stage1 = li_sra_flops(H // 4, W // 4,
+                                  model.block1[0].attn.dim) * len(model.block1)
+            stage2 = li_sra_flops(H // 8, W // 8,
+                                  model.block2[0].attn.dim) * len(model.block2)
+            stage3 = li_sra_flops(H // 16, W // 16,
+                                  model.block3[0].attn.dim) * len(model.block3)
+            stage4 = li_sra_flops(H // 32, W // 32,
+                                  model.block4[0].attn.dim) * len(model.block4)
+        else:  # calculate flops of PVT/PVTv2
+            stage1 = sra_flops(H // 4, W // 4,
+                               model.block1[0].attn.sr_ratio,
+                               model.block1[0].attn.dim) * len(model.block1)
+            stage2 = sra_flops(H // 8, W // 8,
+                               model.block2[0].attn.sr_ratio,
+                               model.block2[0].attn.dim) * len(model.block2)
+            stage3 = sra_flops(H // 16, W // 16,
+                               model.block3[0].attn.sr_ratio,
+                               model.block3[0].attn.dim) * len(model.block3)
+            stage4 = sra_flops(H // 32, W // 32,
+                               model.block4[0].attn.sr_ratio,
+                               model.block4[0].attn.dim) * len(model.block4)
+        flops += stage1 + stage2 + stage3 + stage4
     return flops_to_string(flops), params_to_string(params)
 
 
-def get_pvt_flops(net, input_shape):
-    flops, params = get_model_complexity_info(net, input_shape, as_strings=False)
-    _, H, W = input_shape
-    stage1 = sra_flops(H // 4, W // 4,
-                       net.block1[0].attn.sr_ratio,
-                       net.block1[0].attn.dim,
-                       net.block1[0].attn.num_heads) * len(net.block1)
-    stage2 = sra_flops(H // 8, W // 8,
-                       net.block2[0].attn.sr_ratio,
-                       net.block2[0].attn.dim,
-                       net.block2[0].attn.num_heads) * len(net.block2)
-    stage3 = sra_flops(H // 16, W // 16,
-                       net.block3[0].attn.sr_ratio,
-                       net.block3[0].attn.dim,
-                       net.block3[0].attn.num_heads) * len(net.block3)
-    stage4 = sra_flops(H // 32, W // 32,
-                       net.block4[0].attn.sr_ratio,
-                       net.block4[0].attn.dim,
-                       net.block4[0].attn.num_heads) * len(net.block4)
-    print(stage1 + stage2 + stage3 + stage4)
-    flops += stage1 + stage2 + stage3 + stage4
-    return flops_to_string(flops), params_to_string(params)
+def main():
+    args = parse_args()
 
+    if len(args.shape) == 1:
+        input_shape = (3, args.shape[0], args.shape[0])
+    elif len(args.shape) == 2:
+        input_shape = (3,) + tuple(args.shape)
+    else:
+        raise ValueError('invalid input shape')
 
-def get_pvt_li_flops(net, input_shape):
-    flops, params = get_model_complexity_info(net, input_shape, as_strings=False)
-    _, H, W = input_shape
-    stage1 = lisra_flops(H // 4, W // 4,
-                         net.block1[0].attn.dim) * len(net.block1)
-    stage2 = lisra_flops(H // 8, W // 8,
-                         net.block2[0].attn.dim) * len(net.block2)
-    stage3 = lisra_flops(H // 16, W // 16,
-                         net.block3[0].attn.dim) * len(net.block3)
-    stage4 = lisra_flops(H // 32, W // 32,
-                         net.block4[0].attn.dim) * len(net.block4)
-    print(stage1 + stage2 + stage3 + stage4)
-    flops += stage1 + stage2 + stage3 + stage4
-    return flops_to_string(flops), params_to_string(params)
+    model = create_model(
+        args.model,
+        pretrained=False,
+        num_classes=1000
+    )
+    model.name = args.model
+    if torch.cuda.is_available():
+        model.cuda()
+    model.eval()
 
-
-with torch.cuda.device(0):
-    # calculate vit flops
-    # net = models.vit_small_patch16_224()
-    # input_shape = (3, 224, 224)
-    # flops, params = get_vit_flops(net, input_shape, 16)
-
-    # calculate pvtv1 flops
-    # net = pvt.pvt_small()
-    # input_shape = (3, 224, 224)
-    # flops, params = get_pvt_flops(net, input_shape)
-
-    # calculate pvtv2 flops
-    # net = pvt_v2.pvt_v2_b2()
-    # input_shape = (3, 224, 224)
-    # flops, params = get_pvt_flops(net, input_shape)
-
-    # calculate pvtv2-li flops
-    net = pvt_v2.pvt_v2_b2_li()
-    input_shape = (3, 224, 224)
-    flops, params = get_pvt_li_flops(net, input_shape)
+    flops, params = get_flops(model, input_shape)
 
     split_line = '=' * 30
     print(f'{split_line}\nInput shape: {input_shape}\n'
           f'Flops: {flops}\nParams: {params}\n{split_line}')
+    print('!!!Please be cautious if you use the results in papers. '
+          'You may need to check if all ops are supported and verify that the '
+          'flops computation is correct.')
+
+
+if __name__ == '__main__':
+    main()
--- detection/get_flops.py
+++ detection/get_flops.py
@@ -37,78 +37,45 @@ def parse_args():
     return args
 
 
-def mha_flops(h, w, dim, num_heads):
-    dim_h = dim / num_heads
-    n = h * w
-    f1 = n * dim_h * n * num_heads
-    f2 = n * n * dim_h * num_heads
-    return f1 + f2
+def sra_flops(h, w, r, dim):
+    return 2 * h * w * (h // r) * (w // r) * dim
 
 
-def sra_flops(h, w, r, dim, num_heads):
-    dim_h = dim / num_heads
-    n1 = h * w
-    n2 = h / r * w / r
-
-    f1 = n1 * dim_h * n2 * num_heads
-    f2 = n1 * n2 * dim_h * num_heads
-
-    return f1 + f2
-
-
-def lisra_flops(h, w, dim):
+def li_sra_flops(h, w, dim):
     return 2 * h * w * 7 * 7 * dim
 
 
-def get_vit_flops(net, input_shape, patch_size):
-    flops, params = get_model_complexity_info(net, input_shape, as_strings=False)
-    _, H, W = input_shape
-    stage = mha_flops(H // patch_size, W // patch_size,
-                      net.blocks[0].attn.dim,
-                      net.blocks[0].attn.num_heads) * len(net.blocks)
-    flops += stage
-    return flops_to_string(flops), params_to_string(params)
+def get_flops(model, input_shape):
+    flops, params = get_model_complexity_info(model, input_shape, as_strings=False)
 
+    backbone = model.backbone
+    backbone_name = type(backbone).__name__
 
-def get_pvt_flops(net, input_shape):
-    flops, params = get_model_complexity_info(net, input_shape, as_strings=False)
-
-    net = net.backbone
-    _, H, W = input_shape
-    stage1 = sra_flops(H // 4, W // 4,
-                       net.block1[0].attn.sr_ratio,
-                       net.block1[0].attn.dim,
-                       net.block1[0].attn.num_heads) * len(net.block1)
-    stage2 = sra_flops(H // 8, W // 8,
-                       net.block2[0].attn.sr_ratio,
-                       net.block2[0].attn.dim,
-                       net.block2[0].attn.num_heads) * len(net.block2)
-    stage3 = sra_flops(H // 16, W // 16,
-                       net.block3[0].attn.sr_ratio,
-                       net.block3[0].attn.dim,
-                       net.block3[0].attn.num_heads) * len(net.block3)
-    stage4 = sra_flops(H // 32, W // 32,
-                       net.block4[0].attn.sr_ratio,
-                       net.block4[0].attn.dim,
-                       net.block4[0].attn.num_heads) * len(net.block4)
-    flops += stage1 + stage2 + stage3 + stage4
-    return flops_to_string(flops), params_to_string(params)
-
-
-def get_pvt_li_flops(net, input_shape):
-    flops, params = get_model_complexity_info(net, input_shape, as_strings=False)
-
-    net = net.backbone
-    _, H, W = input_shape
-    stage1 = lisra_flops(H // 4, W // 4,
-                         net.block1[0].attn.dim) * len(net.block1)
-    stage2 = lisra_flops(H // 8, W // 8,
-                         net.block2[0].attn.dim) * len(net.block2)
-    stage3 = lisra_flops(H // 16, W // 16,
-                         net.block3[0].attn.dim) * len(net.block3)
-    stage4 = lisra_flops(H // 32, W // 32,
-                         net.block4[0].attn.dim) * len(net.block4)
-    flops += stage1 + stage2 + stage3 + stage4
+    if 'pvt' in backbone_name:
+        _, H, W = input_shape
+        if 'li' in backbone_name:  # calculate flops of PVTv2_li
+            stage1 = li_sra_flops(H // 4, W // 4,
+                                  backbone.block1[0].attn.dim) * len(backbone.block1)
+            stage2 = li_sra_flops(H // 8, W // 8,
+                                  backbone.block2[0].attn.dim) * len(backbone.block2)
+            stage3 = li_sra_flops(H // 16, W // 16,
+                                  backbone.block3[0].attn.dim) * len(backbone.block3)
+            stage4 = li_sra_flops(H // 32, W // 32,
+                                  backbone.block4[0].attn.dim) * len(backbone.block4)
+        else:  # calculate flops of PVT/PVTv2
+            stage1 = sra_flops(H // 4, W // 4,
+                               backbone.block1[0].attn.sr_ratio,
+                               backbone.block1[0].attn.dim) * len(backbone.block1)
+            stage2 = sra_flops(H // 8, W // 8,
+                               backbone.block2[0].attn.sr_ratio,
+                               backbone.block2[0].attn.dim) * len(backbone.block2)
+            stage3 = sra_flops(H // 16, W // 16,
+                               backbone.block3[0].attn.sr_ratio,
+                               backbone.block3[0].attn.dim) * len(backbone.block3)
+            stage4 = sra_flops(H // 32, W // 32,
+                               backbone.block4[0].attn.sr_ratio,
+                               backbone.block4[0].attn.dim) * len(backbone.block4)
+        flops += stage1 + stage2 + stage3 + stage4
     return flops_to_string(flops), params_to_string(params)
 
 
@@ -144,14 +111,8 @@ def main():
         raise NotImplementedError(
             'FLOPs counter is currently not currently supported with {}'.
                 format(model.__class__.__name__))
-    # calculate CNN-based models' flops
-    # flops, params = get_model_complexity_info(model, input_shape)
 
-    # calculate pvtv1/pvtv2 flops
-    # flops, params = get_pvt_flops(model, input_shape)
-
-    # calculate pvtv2-li flops
-    flops, params = get_pvt_li_flops(model, input_shape)
+    flops, params = get_flops(model, input_shape)
 
     split_line = '=' * 30
     print(f'{split_line}\nInput shape: {input_shape}\n'
