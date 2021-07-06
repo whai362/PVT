28a500c30bbd3a0f36cc93a27df1d5f568d005cc
whai362 <wangwenhai362>
2021-07-06 16:42:57 +0800

    fix finetune bug

--- classification/main.py
+++ classification/main.py
@@ -257,6 +257,26 @@ def main(args):
         drop_block_rate=None,
     )
 
+
+    if args.finetune:
+        if args.finetune.startswith('https'):
+            checkpoint = torch.hub.load_state_dict_from_url(
+                args.finetune, map_location='cpu', check_hash=True)
+        else:
+            checkpoint = torch.load(args.finetune, map_location='cpu')
+
+        if 'model' in checkpoint:
+            checkpoint_model = checkpoint['model']
+        else:
+            checkpoint_model = checkpoint
+        state_dict = model.state_dict()
+        for k in ['head.weight', 'head.bias', 'head_dist.weight', 'head_dist.bias']:
+            if k in checkpoint_model and checkpoint_model[k].shape != state_dict[k].shape:
+                print(f"Removing key {k} from pretrained checkpoint")
+                del checkpoint_model[k]
+
+        model.load_state_dict(checkpoint_model, strict=False)
+
     model.to(device)
 
     model_ema = None
@@ -274,45 +294,6 @@ def main(args):
         model_without_ddp = model.module
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('number of params:', n_parameters)
-
-    if args.finetune:
-        if args.finetune.startswith('https'):
-            checkpoint = torch.hub.load_state_dict_from_url(
-                args.finetune, map_location='cpu', check_hash=True)
-        else:
-            checkpoint = torch.load(args.finetune, map_location='cpu')
-
-        if 'model' in checkpoint:
-            model_without_ddp.load_state_dict(checkpoint['model'])
-        else:
-            model_without_ddp.load_state_dict(checkpoint)
-        state_dict = model.state_dict()
-        for k in ['head.weight', 'head.bias', 'head_dist.weight', 'head_dist.bias']:
-            if k in checkpoint_model and checkpoint_model[k].shape != state_dict[k].shape:
-                print(f"Removing key {k} from pretrained checkpoint")
-                del checkpoint_model[k]
-
-        # # interpolate position embedding
-        # pos_embed_checkpoint = checkpoint_model['pos_embed']
-        # embedding_size = pos_embed_checkpoint.shape[-1]
-        # num_patches = model.patch_embed.num_patches
-        # num_extra_tokens = model.pos_embed.shape[-2] - num_patches
-        # # height (== width) for the checkpoint position embedding
-        # orig_size = int((pos_embed_checkpoint.shape[-2] - num_extra_tokens) ** 0.5)
-        # # height (== width) for the new position embedding
-        # new_size = int(num_patches ** 0.5)
-        # # class_token and dist_token are kept unchanged
-        # extra_tokens = pos_embed_checkpoint[:, :num_extra_tokens]
-        # # only the position tokens are interpolated
-        # pos_tokens = pos_embed_checkpoint[:, num_extra_tokens:]
-        # pos_tokens = pos_tokens.reshape(-1, orig_size, orig_size, embedding_size).permute(0, 3, 1, 2)
-        # pos_tokens = torch.nn.functional.interpolate(
-        #     pos_tokens, size=(new_size, new_size), mode='bicubic', align_corners=False)
-        # pos_tokens = pos_tokens.permute(0, 2, 3, 1).flatten(1, 2)
-        # new_pos_embed = torch.cat((extra_tokens, pos_tokens), dim=1)
-        # checkpoint_model['pos_embed'] = new_pos_embed
-
-        model.load_state_dict(checkpoint_model, strict=False)
 
     linear_scaled_lr = args.lr * args.batch_size * utils.get_world_size() / 512.0
     args.lr = linear_scaled_lr
