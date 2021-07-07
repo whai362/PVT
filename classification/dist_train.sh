fb9adbca186961986b730ca4a0bb5a06ffc66f7b
whai362 <wangwenhai362@163.com>
2021-07-07 15:47:58 +0800

    Update dist_train.sh

--- classification/dist_train.sh
+++ classification/dist_train.sh
@@ -6,4 +6,4 @@ GPUS=$2
 PORT=${PORT:-6666}
 
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    --use_env main.py --config $CONFIG --data-path ${@:3}
+    --use_env main.py --config $CONFIG
