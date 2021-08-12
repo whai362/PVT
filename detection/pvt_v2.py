df6fd1a1822f6385b032d7c0e5d0b4bf0aa620ce
Hu Ye <xiaohuzc@gmail.com>
2021-08-12 20:42:03 +0800

    fix bug in OverlapPatchEmbed

--- detection/pvt_v2.py
+++ detection/pvt_v2.py
@@ -180,7 +180,7 @@ class OverlapPatchEmbed(nn.Module):
 
         self.img_size = img_size
         self.patch_size = patch_size
-        self.H, self.W = img_size[0] // patch_size[0], img_size[1] // patch_size[1]
+        self.H, self.W = img_size[0] // stride, img_size[1] // stride
         self.num_patches = self.H * self.W
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=stride,
                               padding=(patch_size[0] // 2, patch_size[1] // 2))
